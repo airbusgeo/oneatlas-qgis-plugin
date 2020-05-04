647775dd6fdce4f568173ebaa14de473438bf76b
herve31 <59444557+herve31@users.noreply.github.com>
2020-05-04 09:56:21 +0200

    Part of issue #3, #4
    #3 : Display new option for PMS when displaying OGC protocol.
    #4 : Raise warning for Archive imagery if trying to display WMTS

--- oneAtlas.py
+++ oneAtlas.py
@@ -2,6 +2,7 @@ import os
 
 from qgis.PyQt.QtWidgets import QAction, QMessageBox, QRadioButton, QGridLayout, QGroupBox
 from qgis.PyQt.QtGui import QIcon, QColor
+from qgis.core import Qgis
 
 from qgis.core import (
     QgsVectorLayer,
@@ -86,6 +87,9 @@ class OneAtlas:
 
     # Stream image
     def stream(self):
+
+        goForStream = True
+
         # Avoid auth exception if you reopen the project and don't open the search
         if self.mySearch == None:
             self.mySearch = MySearch(self.iface)
@@ -118,72 +122,93 @@ class OneAtlas:
 
             # Dialog for image choice (panchro/multi & wmts/wcs)
             self.msgBox = QMessageBox()
-            self.msgBox.setWindowTitle('Airbus')
+            self.msgBox.setWindowTitle(self.menu)
 
-            protocolGroup = QGroupBox('Protocol')
-            protocolGrid = QGridLayout()
-            wmtsRadio = QRadioButton('WMTS')
-            wcsRadio = QRadioButton('WCS')
-            protocolGrid.addWidget(wmtsRadio, 0, 0)
-            protocolGrid.addWidget(wcsRadio, 0, 1)
-            protocolGroup.setLayout(protocolGrid)
-            
-            styleGroup = QGroupBox('Style')
-            styleGrid = QGridLayout()
-            multispectralRadio = QRadioButton('multispectral')
-            panchromaticRadio = QRadioButton('panchromatic')
-            styleGrid.addWidget(multispectralRadio, 0, 0)
-            styleGrid.addWidget(panchromaticRadio, 0, 1)
-            styleGroup.setLayout(styleGrid)
-
-            self.msgBox.layout().addWidget(protocolGroup, 0, 0)
-            self.msgBox.layout().addWidget(styleGroup, 1, 0)
-
-            wmtsRadio.setChecked(True)
-            if type(self.selectedFeature['wcs_multispectral']) != str:
-                protocolGroup.setEnabled(False)
-
-            multispectralRadio.setChecked(True)
-
-            self.msgBox.setStandardButtons(QMessageBox.Abort | QMessageBox.Ok)
-            reply = self.msgBox.exec_()
-            if reply == QMessageBox.Abort:
-                return
-            if wmtsRadio.isChecked():
-                urlAttr = 'wmts_'
-                layers = 'default'
-                styles = 'rgb'
-                tileMatrixSet = 'EPSG4326'
+            if self.selectedFeature['processingLevel'] == 'ALBUM':
+                self.iface.messageBar().pushMessage("Warning", "The feature can't be displayed (no WMTS for Archive features)", level=Qgis.Warning)
+                goForStream = False
             else:
-                urlAttr = 'wcs_'
-                isWcs = True
-            if multispectralRadio.isChecked():
-                urlAttr += 'multispectral'
-            else:
-                urlAttr += 'panchromatic'
+                protocolGroup = QGroupBox('Protocol')
+                protocolGrid = QGridLayout()
+                wmtsRadio = QRadioButton('WMTS')
+                wcsRadio = QRadioButton('WCS')
+                protocolGrid.addWidget(wmtsRadio, 0, 0)
+                protocolGrid.addWidget(wcsRadio, 0, 1)
+                protocolGroup.setLayout(protocolGrid)
+                
+                styleGroup = QGroupBox('Style')
+                styleGrid = QGridLayout()
+                multispectralRadio = QRadioButton('multispectral')
+                panchromaticRadio = QRadioButton('panchromatic')
+                pmsRadio = QRadioButton('pms')
+                styleGrid.addWidget(multispectralRadio, 0, 0)
+                styleGrid.addWidget(panchromaticRadio, 0, 1)
+                styleGrid.addWidget(pmsRadio, 0, 2)
+                styleGroup.setLayout(styleGrid)
+
+                self.msgBox.layout().addWidget(protocolGroup, 0, 0)
+                self.msgBox.layout().addWidget(styleGroup, 1, 0)
+
+                wmtsRadio.setChecked(True)
+                if type(self.selectedFeature['wcs_multispectral']) != str and type(self.selectedFeature['wcs_panchromatic']) != str and type(self.selectedFeature['wcs_pms']) != str:
+                    protocolGroup.setEnabled(False)
+
+                if type(self.selectedFeature['wmts_pms']) != str:
+                    multispectralRadio.setEnabled(True)
+                    panchromaticRadio.setEnabled(True)
+                    pmsRadio.setEnabled(False)
+                    multispectralRadio.setChecked(True)
+                else:
+                    multispectralRadio.setEnabled(False)
+                    panchromaticRadio.setEnabled(False)
+                    pmsRadio.setEnabled(True)
+                    pmsRadio.setChecked(True)
+
+                
+
+                self.msgBox.setStandardButtons(QMessageBox.Abort | QMessageBox.Ok)
+                reply = self.msgBox.exec_()
+                if reply == QMessageBox.Abort:
+                    return
+                if wmtsRadio.isChecked():
+                    urlAttr = 'wmts_'
+                    layers = 'default'
+                    styles = 'rgb'
+                    tileMatrixSet = 'EPSG4326'
+                else:
+                    urlAttr = 'wcs_'
+                    isWcs = True
+                if multispectralRadio.isChecked():
+                    urlAttr += 'multispectral'
+                elif panchromaticRadio.isChecked():
+                    urlAttr += 'panchromatic'
+                else:
+                    urlAttr += 'pms'
         else:
             self.error(f'Service "{service}" of the feature ocg_fid={self.selectedFeature.id()} isn\'t recognized\nIt should be "Basemap" or "Data"')
             return
 
-        # Add a WMTS raster layer
-        # Order of url parameters are important !
-        # Why layers is required, maybe is an internal id for wmts gesture ?
-        # What is styles ?
-        try:
-            url = self.selectedFeature[urlAttr]
-            name = f'{service} {self.selectedFeature["id"]}'
-            if isWcs:
-                rlayer = QgsRasterLayer(f'dpiMode=7&identifier=default&password={password}&url={url}&username={username}', name, 'wcs')
-            else:
-                rlayer = QgsRasterLayer(f'crs=EPSG:4326&dpiMode=7&format=image/png&layers={layers}&password={password}&styles={styles}&tileMatrixSet={tileMatrixSet}&url={url}&username={username}', name, 'wms')
-        except Exception as e:
-            self.error(f'Error in protocol connection\n\n{str(e)}')
-            return
-        if rlayer.isValid() == False:
-            self.error(f'Raster layer is invalid\n\n{rlayer.error()}')
-            return
 
-        QgsProject.instance().addMapLayer(rlayer)
+        if goForStream:
+            # Add a WMTS raster layer
+            # Order of url parameters are important !
+            # Why layers is required, maybe is an internal id for wmts gesture ?
+            # What is styles ?
+            try:
+                url = self.selectedFeature[urlAttr]
+                name = f'{service} {self.selectedFeature["id"]}'
+                if isWcs:
+                    rlayer = QgsRasterLayer(f'dpiMode=7&identifier=default&password={password}&url={url}&username={username}', name, 'wcs')
+                else:
+                    rlayer = QgsRasterLayer(f'crs=EPSG:4326&dpiMode=7&format=image/png&layers={layers}&password={password}&styles={styles}&tileMatrixSet={tileMatrixSet}&url={url}&username={username}', name, 'wms')
+            except Exception as e:
+                self.error(f'Error in protocol connection\n\n{str(e)}')
+                return
+            if rlayer.isValid() == False:
+                self.error(f'Raster layer is invalid\n\n{rlayer.error()}')
+                return
+
+            QgsProject.instance().addMapLayer(rlayer)
 
     def error(self, msg):
-        QMessageBox.critical(None, 'Airbus', msg)
+        QMessageBox.critical(None, self.menu, msg)
